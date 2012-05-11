7940e6cfafd2f4553280baaae30f5d2f85092302
Jon Buckley <jon@jbuckley.ca>
2012-05-11 16:14:29 -0400

    Send a failed message when linting fails

--- on_cmd_check.js
+++ on_cmd_check.js
@@ -2,8 +2,9 @@ var botio = require(process.env['BOTIO_MODULE']);
 require('shelljs/global');
 
 exec('npm install', {silent: true});
-botio.message("Checking " + botio.head_sha + " of pull request #" + botio.issue);
-exec('node make check');
-exec('node make package');
-cp('-R', 'dist/', botio.public_dir);
+
+var o = exec('node make check').output;
+if (!o.match(/^### Linting JS files\s$/)) {
+  exit(1);
+}
 
