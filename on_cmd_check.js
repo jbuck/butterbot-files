a91a974330c86954b9d7979388c7d0654b4544d8
Jon Buckley <jon@jbuckley.ca>
2012-06-13 12:07:17 -0400

    Update linting for CSS

--- on_cmd_check.js
+++ on_cmd_check.js
@@ -4,7 +4,7 @@ require('shelljs/global');
 exec('npm install', {silent: true});
 
 var o = exec('node make check').output;
-if (!o.match(/^### Linting JS files\s$/)) {
+if (!o.match(/^### Linting JS files\s### Linting CSS files\s$/)) {
   exit(1);
 }
 
