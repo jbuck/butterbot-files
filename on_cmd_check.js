1f60ca882827bfb800dcfd73496bc5106a3f464f
Jon Buckley <jon@jbuckley.ca>
2012-08-03 12:04:52 -0400

    Update check script for new linting message

--- on_cmd_check.js
+++ on_cmd_check.js
@@ -4,7 +4,7 @@ require('shelljs/global');
 exec('npm install', {silent: true});
 
 var o = exec('node make check').output;
-if (!o.match(/^### Linting JS files\s### Linting CSS files\s$/)) {
+if (!o.match(/^### Linting JS files\s### Linting CSS files\s### Linting HTML - requires network access\s$/)) {
   exit(1);
 }
 
