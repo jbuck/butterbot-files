365501e0339ffb504ce8f7ec131bbf95e0af4bd2
Jon Buckley <jon@jbuckley.ca>
2012-08-10 15:26:57 -0400

    Init submodules for external HTML5 linting

--- on_cmd_check.js
+++ on_cmd_check.js
@@ -2,6 +2,7 @@ var botio = require(process.env['BOTIO_MODULE']);
 require('shelljs/global');
 
 exec('npm install', {silent: true});
+exec('git submodule update --init', {silent: true});
 
 var o = exec('node make check').output;
 if (!o.match(/^### Linting JS files\s### Linting CSS files\s### Linting HTML - requires network access\s$/)) {
