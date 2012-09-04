ff36acd2ff52fed1974891dab4ea1399ca35fbba
Jon Buckley <jon@jbuckley.ca>
2012-09-04 12:06:21 -0400

    Add new method of checking for success condition

--- on_cmd_check.js
+++ on_cmd_check.js
@@ -4,8 +4,16 @@ require('shelljs/global');
 exec('npm install', {silent: true});
 exec('git submodule update --init', {silent: true});
 
+var expectedOutput = "### Linting JS files\n" +
+                     "### Building CSS using LESS for css/butter.ui.less (without compression)\n" +
+                     "### Building CSS using LESS for css/transitions.less (without compression)\n" + 
+                     "### Building CSS using LESS for css/embed.less (without compression)\n" +
+                     "### Building CSS using LESS for css/embed-shell.less (without compression)\n" + 
+                     "### Linting CSS files\n" +
+                     "### Linting HTML - requires network access\n\n";
+
 var o = exec('node make check').output;
-if (!o.match(/^### Linting JS files\s### Linting CSS files\s### Linting HTML - requires network access\s$/)) {
+if (o === expectedOutput) {
   exit(1);
 }
 
