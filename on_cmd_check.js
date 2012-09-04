44cbacde7b1f41ec06f8b1a266784ffc7e76f161
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2012-09-04 12:42:56 -0400

    Update check command for changes to butter's make.js

--- on_cmd_check.js
+++ on_cmd_check.js
@@ -3,17 +3,10 @@ require('shelljs/global');
 
 exec('npm install', {silent: true});
 exec('git submodule update --init', {silent: true});
-
-var expectedOutput = "### Linting JS files\n" +
-                     "### Building CSS using LESS for css/butter.ui.less (without compression)\n" +
-                     "### Building CSS using LESS for css/transitions.less (without compression)\n" + 
-                     "### Building CSS using LESS for css/embed.less (without compression)\n" +
-                     "### Building CSS using LESS for css/embed-shell.less (without compression)\n" + 
-                     "### Linting CSS files\n" +
-                     "### Linting HTML - requires network access\n\n";
-
 var o = exec('node make check').output;
-if (o === expectedOutput) {
+
+// Unless we get any console spew, and therefore an error,
+// this run passed.
+if (o === "") {
   exit(1);
 }
-
